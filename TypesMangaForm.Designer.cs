72fc9d0c24b2f4f771dce7f12ef84c8c141a0081
manuela <emkengmo02@monccnb.ca>
2024-09-17 08:48:13 -0300

    UI

--- TypesMangaForm.Designer.cs
+++ TypesMangaForm.Designer.cs
@@ -29,62 +29,44 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TypesMangaForm));
-            this.logoMangaPictureBox = new System.Windows.Forms.PictureBox();
-            this.mangaPictureBox = new System.Windows.Forms.PictureBox();
             this.TitreLabel = new System.Windows.Forms.Label();
             this.descriptionLabel = new System.Windows.Forms.Label();
             this.classificationLabel = new System.Windows.Forms.Label();
             this.typesMangaListBox = new System.Windows.Forms.ListBox();
             this.impressionGroupBox = new System.Windows.Forms.GroupBox();
-            this.logoRadioButton = new System.Windows.Forms.RadioButton();
-            this.imageRadioButton = new System.Windows.Forms.RadioButton();
             this.imprimerButton = new System.Windows.Forms.Button();
+            this.imageRadioButton = new System.Windows.Forms.RadioButton();
+            this.logoRadioButton = new System.Windows.Forms.RadioButton();
             this.fermerButton = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            ((System.ComponentModel.ISupportInitialize)(this.logoMangaPictureBox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.mangaPictureBox)).BeginInit();
+            this.siteLabel = new System.Windows.Forms.Label();
+            this.programmeurLabel = new System.Windows.Forms.Label();
+            this.liensListBox = new System.Windows.Forms.ListBox();
+            this.mangaPictureBox = new System.Windows.Forms.PictureBox();
+            this.logoMangaPictureBox = new System.Windows.Forms.PictureBox();
             this.impressionGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mangaPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logoMangaPictureBox)).BeginInit();
             this.SuspendLayout();
-            // 
-            // logoMangaPictureBox
-            // 
-            this.logoMangaPictureBox.Image = global::TypesManga.Properties.Resources.Logo_mangaschaine2022;
-            this.logoMangaPictureBox.Location = new System.Drawing.Point(260, 30);
-            this.logoMangaPictureBox.Name = "logoMangaPictureBox";
-            this.logoMangaPictureBox.Size = new System.Drawing.Size(171, 69);
-            this.logoMangaPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.logoMangaPictureBox.TabIndex = 0;
-            this.logoMangaPictureBox.TabStop = false;
-            // 
-            // mangaPictureBox
-            // 
-            this.mangaPictureBox.Image = global::TypesManga.Properties.Resources.mha_com;
-            this.mangaPictureBox.Location = new System.Drawing.Point(43, 340);
-            this.mangaPictureBox.Name = "mangaPictureBox";
-            this.mangaPictureBox.Size = new System.Drawing.Size(260, 214);
-            this.mangaPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.mangaPictureBox.TabIndex = 1;
-            this.mangaPictureBox.TabStop = false;
             // 
             // TitreLabel
             // 
             this.TitreLabel.AutoSize = true;
-            this.TitreLabel.Font = new System.Drawing.Font("Pristina", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitreLabel.Font = new System.Drawing.Font("Pristina", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TitreLabel.ForeColor = System.Drawing.Color.Firebrick;
-            this.TitreLabel.Location = new System.Drawing.Point(454, 41);
+            this.TitreLabel.Location = new System.Drawing.Point(624, 62);
+            this.TitreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.TitreLabel.Name = "TitreLabel";
-            this.TitreLabel.Size = new System.Drawing.Size(274, 58);
+            this.TitreLabel.Size = new System.Drawing.Size(334, 75);
             this.TitreLabel.TabIndex = 2;
             this.TitreLabel.Text = "Types de manga";
             // 
             // descriptionLabel
             // 
             this.descriptionLabel.AutoSize = true;
-            this.descriptionLabel.Location = new System.Drawing.Point(330, 340);
+            this.descriptionLabel.Location = new System.Drawing.Point(454, 510);
+            this.descriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.descriptionLabel.Name = "descriptionLabel";
-            this.descriptionLabel.Size = new System.Drawing.Size(376, 112);
+            this.descriptionLabel.Size = new System.Drawing.Size(549, 175);
             this.descriptionLabel.TabIndex = 3;
             this.descriptionLabel.Text = resources.GetString("descriptionLabel.Text");
             // 
@@ -93,17 +75,17 @@
             this.classificationLabel.AutoSize = true;
             this.classificationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.classificationLabel.ForeColor = System.Drawing.Color.Firebrick;
-            this.classificationLabel.Location = new System.Drawing.Point(39, 139);
+            this.classificationLabel.Location = new System.Drawing.Point(54, 208);
+            this.classificationLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.classificationLabel.Name = "classificationLabel";
-            this.classificationLabel.Size = new System.Drawing.Size(130, 20);
+            this.classificationLabel.Size = new System.Drawing.Size(176, 29);
             this.classificationLabel.TabIndex = 5;
             this.classificationLabel.Text = "Classification:";
-            this.classificationLabel.Click += new System.EventHandler(this.classificationLabel_Click);
             // 
             // typesMangaListBox
             // 
             this.typesMangaListBox.FormattingEnabled = true;
-            this.typesMangaListBox.ItemHeight = 16;
+            this.typesMangaListBox.ItemHeight = 24;
             this.typesMangaListBox.Items.AddRange(new object[] {
             "Il existe plusieurs types de manga:",
             "- Shonen Manga",
@@ -112,9 +94,10 @@
             "- Josei Manga",
             "- Yaoi Manga",
             "- Yuri Manga"});
-            this.typesMangaListBox.Location = new System.Drawing.Point(41, 187);
+            this.typesMangaListBox.Location = new System.Drawing.Point(56, 280);
+            this.typesMangaListBox.Margin = new System.Windows.Forms.Padding(4);
             this.typesMangaListBox.Name = "typesMangaListBox";
-            this.typesMangaListBox.Size = new System.Drawing.Size(665, 132);
+            this.typesMangaListBox.Size = new System.Drawing.Size(913, 196);
             this.typesMangaListBox.TabIndex = 8;
             // 
             // impressionGroupBox
@@ -122,97 +105,130 @@
             this.impressionGroupBox.Controls.Add(this.imprimerButton);
             this.impressionGroupBox.Controls.Add(this.imageRadioButton);
             this.impressionGroupBox.Controls.Add(this.logoRadioButton);
-            this.impressionGroupBox.Location = new System.Drawing.Point(753, 340);
+            this.impressionGroupBox.Location = new System.Drawing.Point(1035, 510);
+            this.impressionGroupBox.Margin = new System.Windows.Forms.Padding(4);
             this.impressionGroupBox.Name = "impressionGroupBox";
-            this.impressionGroupBox.Size = new System.Drawing.Size(178, 194);
+            this.impressionGroupBox.Padding = new System.Windows.Forms.Padding(4);
+            this.impressionGroupBox.Size = new System.Drawing.Size(245, 291);
             this.impressionGroupBox.TabIndex = 9;
             this.impressionGroupBox.TabStop = false;
             this.impressionGroupBox.Text = "Impression";
             // 
-            // logoRadioButton
+            // imprimerButton
             // 
-            this.logoRadioButton.AutoSize = true;
-            this.logoRadioButton.Location = new System.Drawing.Point(30, 44);
-            this.logoRadioButton.Name = "logoRadioButton";
-            this.logoRadioButton.Size = new System.Drawing.Size(59, 20);
-            this.logoRadioButton.TabIndex = 0;
-            this.logoRadioButton.TabStop = true;
-            this.logoRadioButton.Text = "Logo";
-            this.logoRadioButton.UseVisualStyleBackColor = true;
+            this.imprimerButton.Location = new System.Drawing.Point(41, 202);
+            this.imprimerButton.Margin = new System.Windows.Forms.Padding(4);
+            this.imprimerButton.Name = "imprimerButton";
+            this.imprimerButton.Size = new System.Drawing.Size(157, 57);
+            this.imprimerButton.TabIndex = 2;
+            this.imprimerButton.Text = "&Imprimer";
+            this.imprimerButton.UseVisualStyleBackColor = true;
+            this.imprimerButton.Click += new System.EventHandler(this.imprimerButton_Click);
             // 
             // imageRadioButton
             // 
             this.imageRadioButton.AutoSize = true;
-            this.imageRadioButton.Location = new System.Drawing.Point(30, 86);
+            this.imageRadioButton.Location = new System.Drawing.Point(41, 129);
+            this.imageRadioButton.Margin = new System.Windows.Forms.Padding(4);
             this.imageRadioButton.Name = "imageRadioButton";
-            this.imageRadioButton.Size = new System.Drawing.Size(66, 20);
+            this.imageRadioButton.Size = new System.Drawing.Size(91, 29);
             this.imageRadioButton.TabIndex = 1;
             this.imageRadioButton.TabStop = true;
             this.imageRadioButton.Text = "Image";
             this.imageRadioButton.UseVisualStyleBackColor = true;
             // 
-            // imprimerButton
+            // logoRadioButton
             // 
-            this.imprimerButton.Location = new System.Drawing.Point(30, 135);
-            this.imprimerButton.Name = "imprimerButton";
-            this.imprimerButton.Size = new System.Drawing.Size(114, 38);
-            this.imprimerButton.TabIndex = 2;
-            this.imprimerButton.Text = "&Imprimer";
-            this.imprimerButton.UseVisualStyleBackColor = true;
+            this.logoRadioButton.AutoSize = true;
+            this.logoRadioButton.Location = new System.Drawing.Point(41, 66);
+            this.logoRadioButton.Margin = new System.Windows.Forms.Padding(4);
+            this.logoRadioButton.Name = "logoRadioButton";
+            this.logoRadioButton.Size = new System.Drawing.Size(81, 29);
+            this.logoRadioButton.TabIndex = 0;
+            this.logoRadioButton.TabStop = true;
+            this.logoRadioButton.Text = "Logo";
+            this.logoRadioButton.UseVisualStyleBackColor = true;
             // 
             // fermerButton
             // 
-            this.fermerButton.Location = new System.Drawing.Point(817, 749);
+            this.fermerButton.Location = new System.Drawing.Point(1133, 1108);
+            this.fermerButton.Margin = new System.Windows.Forms.Padding(4);
             this.fermerButton.Name = "fermerButton";
-            this.fermerButton.Size = new System.Drawing.Size(114, 38);
+            this.fermerButton.Size = new System.Drawing.Size(157, 57);
             this.fermerButton.TabIndex = 3;
             this.fermerButton.Text = "Fermer";
             this.fermerButton.UseVisualStyleBackColor = true;
+            this.fermerButton.Click += new System.EventHandler(this.fermerButton_Click);
             // 
-            // label2
+            // siteLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.Firebrick;
-            this.label2.Location = new System.Drawing.Point(39, 586);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(217, 20);
-            this.label2.TabIndex = 10;
-            this.label2.Text = "Sites relies aux mangas:";
+            this.siteLabel.AutoSize = true;
+            this.siteLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siteLabel.ForeColor = System.Drawing.Color.Firebrick;
+            this.siteLabel.Location = new System.Drawing.Point(54, 879);
+            this.siteLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.siteLabel.Name = "siteLabel";
+            this.siteLabel.Size = new System.Drawing.Size(297, 29);
+            this.siteLabel.TabIndex = 10;
+            this.siteLabel.Text = "Sites relies aux mangas:";
             // 
-            // label3
+            // programmeurLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.Green;
-            this.label3.Location = new System.Drawing.Point(39, 757);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(353, 20);
-            this.label3.TabIndex = 11;
-            this.label3.Text = "Programmeur: Andreas, Dylane, Manuela";
+            this.programmeurLabel.AutoSize = true;
+            this.programmeurLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.programmeurLabel.ForeColor = System.Drawing.Color.Green;
+            this.programmeurLabel.Location = new System.Drawing.Point(63, 1120);
+            this.programmeurLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.programmeurLabel.Name = "programmeurLabel";
+            this.programmeurLabel.Size = new System.Drawing.Size(573, 29);
+            this.programmeurLabel.TabIndex = 11;
+            this.programmeurLabel.Text = "Programmeur: Andreas, Cedric,Dylane, Manuela";
             // 
-            // listBox1
+            // liensListBox
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Items.AddRange(new object[] {
+            this.liensListBox.FormattingEnabled = true;
+            this.liensListBox.ItemHeight = 24;
+            this.liensListBox.Items.AddRange(new object[] {
             "https://harajuku-store.com/blogs/harajuku-blog/type-de-manga",
             "https://www.manga-news.com/",
             "https://v5.voiranime.com/",
             "https://mangadex.org/"});
-            this.listBox1.Location = new System.Drawing.Point(43, 629);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(888, 84);
-            this.listBox1.TabIndex = 12;
+            this.liensListBox.Location = new System.Drawing.Point(59, 944);
+            this.liensListBox.Margin = new System.Windows.Forms.Padding(4);
+            this.liensListBox.Name = "liensListBox";
+            this.liensListBox.Size = new System.Drawing.Size(1220, 124);
+            this.liensListBox.TabIndex = 12;
+            // 
+            // mangaPictureBox
+            // 
+            this.mangaPictureBox.Image = global::TypesManga.Properties.Resources.mha_com;
+            this.mangaPictureBox.Location = new System.Drawing.Point(59, 510);
+            this.mangaPictureBox.Margin = new System.Windows.Forms.Padding(4);
+            this.mangaPictureBox.Name = "mangaPictureBox";
+            this.mangaPictureBox.Size = new System.Drawing.Size(358, 321);
+            this.mangaPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mangaPictureBox.TabIndex = 1;
+            this.mangaPictureBox.TabStop = false;
+            // 
+            // logoMangaPictureBox
+            // 
+            this.logoMangaPictureBox.Image = global::TypesManga.Properties.Resources.Logo_mangaschaine2022;
+            this.logoMangaPictureBox.Location = new System.Drawing.Point(358, 45);
+            this.logoMangaPictureBox.Margin = new System.Windows.Forms.Padding(4);
+            this.logoMangaPictureBox.Name = "logoMangaPictureBox";
+            this.logoMangaPictureBox.Size = new System.Drawing.Size(235, 104);
+            this.logoMangaPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logoMangaPictureBox.TabIndex = 0;
+            this.logoMangaPictureBox.TabStop = false;
             // 
             // TypesMangaForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(989, 817);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(1373, 1190);
+            this.Controls.Add(this.liensListBox);
+            this.Controls.Add(this.programmeurLabel);
+            this.Controls.Add(this.siteLabel);
             this.Controls.Add(this.fermerButton);
             this.Controls.Add(this.impressionGroupBox);
             this.Controls.Add(this.typesMangaListBox);
@@ -221,13 +237,15 @@
             this.Controls.Add(this.TitreLabel);
             this.Controls.Add(this.mangaPictureBox);
             this.Controls.Add(this.logoMangaPictureBox);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "TypesMangaForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Types de manga";
-            ((System.ComponentModel.ISupportInitialize)(this.logoMangaPictureBox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.mangaPictureBox)).EndInit();
+            this.Load += new System.EventHandler(this.TypesMangaForm_Load);
             this.impressionGroupBox.ResumeLayout(false);
             this.impressionGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mangaPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logoMangaPictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -246,9 +264,10 @@
         private System.Windows.Forms.RadioButton imageRadioButton;
         private System.Windows.Forms.RadioButton logoRadioButton;
         private System.Windows.Forms.Button fermerButton;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label siteLabel;
+        private System.Windows.Forms.Label programmeurLabel;
+        private System.Windows.Forms.ListBox liensListBox;
+       // private Microsoft.VisualBasic.PowerPacks.Printing.PrintForm typeMangaPrintForm;
     }
 }
 
